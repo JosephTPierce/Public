f39795f91fdfe3fb739c3e3b3a080492678d43a6
Tyler <pierce.josepht@gmail.com>
2025-12-13 12:31:46 -0500

    Added Skeleton

---
+++ Network Scanner/main.py
@@ -0,0 +1,17 @@
+# Skeleton:
+
+# Present a home screen that tells who its made by and askes for a url or IP address to scan
+
+# Take the input and verify that its a valid IP or if they entered in a URL verify and give back the IP --- maybe ping it to make sure we are able to connect to the host ??? (it would also be cool to see the steps when running)
+    # if the user types in a url convert that url into and ip address
+        # Assign target to the IP
+
+# Scan the ports for that IP using multi-treading so its fast
+
+# Present what ports are open to the user 
+
+# Ask the user if they want to return to the home screen to allow them to run the program again or if they want to exit. 
+
+
+# ----- CODE -----
+
