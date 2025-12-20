82693e8a4268854a1b629caed1a460e316a0b629
Tyler <pierce.josepht@gmail.com>
2025-12-20 11:24:44 -0500

    Added To-Do

--- Network Scanner/main.py
+++ Network Scanner/main.py
@@ -1,19 +1,7 @@
-# Skeleton:
-
-# Present a home screen that tells who its made by and askes for a url or IP address to scan
-
-# Take the input and verify that its a valid IP or if they entered in a URL verify and give back the IP --- maybe ping it to make sure we are able to connect to the host ??? (it would also be cool to see the steps when running)
-    # if the user types in a url convert that url into and ip address
-        # Assign target to the IP
-
-# Scan the ports for that IP using multi-treading so its fast
-
-# Present what ports are open to the user 
-
-# Ask the user if they want to return to the home screen to allow them to run the program again or if they want to exit. 
-
-
-# ----- CODE -----
+# TO-DO:
+# Make it so the scanner also tells you the service the port is running and what version it is
+# Allow export to CSV
+# Make a prettier home screen 
 
 
 import socket
