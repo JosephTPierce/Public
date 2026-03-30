7f8a14b1727a85851f13206d7086e30c9ab23bcb
Tyler Pierce <pierce.josepht@gmail.com>
2026-03-30 15:13:34 -0400

    Now uses winget to install adobe

--- Device Setup/main.ps1
+++ Device Setup/main.ps1
@@ -3,14 +3,7 @@
 # --------------------------------
 
 
-
-
 # ---------- VARS ----------
-
-
-$zipFile = "C:\Users\TylerPierce\Downloads\Acrobat_DC_Web_x64_WWMUI.zip"
-$extractPath = "C:\Users\TylerPierce\Downloads\AdobeInstall"
-$installer = Get-ChildItem -Path $extractPath -Filter "setup.exe" -Recurse | Select-Object -First 1
 
 
 
@@ -25,65 +18,12 @@ function removeApps {
 
 
 
-# WORKS 
-function downloadAdobe {
-    
-    # Target URL
-    $url = "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acrobat_DC_Web_x64_WWMUI.zip"
-
-    # Downloading it
-    Start-BitsTransfer -Source $url -Destination $zipFile
-    Write-Host "Finished downloading Adobe"
+function installAdobe{
+    winget install --id=Adobe.Acrobat.Reader.64-bit -e --silent --accept-package-agreements --accept-source-agreements
+    Write-Host "Adobe Reader installed"
 }
-
-
-
-function installAdobe {
-
-    # Search for installer (setup.exe)
-
-    if ($installer) {
-        Write-Host "Found setup at: $($installer.FullName)"
-
-        # Run installer 
-        Start-Process $installer.FullName -ArgumentList "/sAll /quiet" -Wait
-
-        Write-Host "Adobe instilation complete"
-    }
-    else {
-        Write-Host "Unable to find setup.exe"
-    }
-
-}
-
 
 
 # ---------- MAIN ----------
 
-#downloadAdobe
-#extractingAdobe
 installAdobe
-
-
-
-
-
-
-
-
-
-# ----- UNUSED FUNCTIONS -----
-
-
-# WAS USED WHEN DOWNLOADING A ZIP, NOW DOWNLOADING EXE FILE INSTEAD
- function extractingAdobe {
-
-    # Checks to see if folder exists, if it doesnt then makes it 
-    if (!(Test-Path $extractPath)) {
-        New-Item -ItemType Directory -Path $extractPath
-    }
-
-    # Run command 
-    Expand-Archive -Path $zipFile -DestinationPath $extractPath -Force
-
-}
