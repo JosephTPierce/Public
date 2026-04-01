0a5146b07fe71ee3f484c14081a961d178fcaf36
Tyler Pierce <pierce.josepht@gmail.com>
2026-04-01 12:01:10 -0400

    All functions needed, most working. See comments.

--- Device Setup/main.ps1
+++ Device Setup/main.ps1
@@ -9,9 +9,9 @@
 # - Find a way to pin Outlook and teams to start menu
 # - Find a way to unpin items from taskbar
 # - Find a way to unpin items from start menu
-# - Find a way to open or sign into OneDrive
-# - See if I can set homepage to myapps.microsoft.com in edge
-# - See if we can open rename dialog at the end 
+
+# - Find a way to open or sign into OneDrive -- cant automate 
+# - See if I can set homepage to myapps.microsoft.com in edge -- kinda works. Could just open it instad
 
 
 
@@ -32,8 +32,7 @@ function setTimeZone {
 }
 
 
-
-# Sets power plan to High Performance, doesnt allow user to change it in the future without PS... see if thats okay
+# Do we want to use this? It block the ability to change it in the GUI
 function setPowerPlan {
 
     powercfg /setactive SCHEME_MIN
@@ -52,10 +51,21 @@ function setSleepSetting {
 }
 
 
-# ADD PROGRAMS TO REMOVE
+# What happens if you try to remove an app that doesnt exist?
 function removeApps {
 
+    # Definete removal
+
     winget uninstall "Xbox TCUI"
+    winget uninstall "Xbox Identity Provider"
+    winget uninstall "Xbox"
+    winget uninstall "Game Bar"
+
+
+    # possible removal 
+
+    winget uninstall "Lenovo Vantage Service"
+    winget uninstall "Lenovo Commercial Vantage"
     winget uninstall "Xbox Identity Provider"
 
 }
@@ -64,7 +74,7 @@ function removeApps {
 
 function installAdobe{
 
-    winget install --id=Adobe.Acrobat.Reader.64-bit -e --silent --accept-package-agreements --accept-source-agreements
+    winget install --id=Adobe.Acrobat.Reader.64-bit -e --silent --accept-package-agreements --accept-source-agreements --source winget
 
 }
 
@@ -90,10 +100,43 @@ function installEncompass {
 }
 
 
+# Do we want to do this? It locks out the user from changing it later.
+function setEdgeHomepage {
+    
+    $homepageURL = "https://myapps.microsoft.com"
+
+    $edgePoliciesPath = "HKCU:\SOFTWARE\Policies\Microsoft\Edge"
+
+    # Create policy key if it doesn't exist
+    New-Item -Path $edgePoliciesPath -Force | Out-Null
+
+    # Get edge to open start page
+    Set-ItemProperty -Path $edgePoliciesPath -Name "RestoreOnStartup" -Value 4
+
+    # Make URL list key
+    New-Item -Path "$edgePoliciesPath\RestoreOnStartupURLs" -Force | Out-Null
+
+    # Set homepage URL
+    Set-ItemProperty -Path "$edgePoliciesPath\RestoreOnStartupURLs" -Name "1" -Value $homepageURL
+    
+}
+
+
+# Opens rename menu
+function renameComputer {
+
+    # Opens system menu when you can rename computer and see users name
+    Start-Process "ms-settings:about"
+
+    # Opens rename box but cant see users name 
+    #Start-Process "SystemPropertiesComputerName"
+
+}
 
 # ---------- MAIN ----------
 
-installAdobe
-downloadEncompass
-installEncompass
-setTimeZone
+#installAdobe
+#downloadEncompass
+#installEncompass
+#setTimeZone
+#renameComputer
