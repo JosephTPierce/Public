430eb2e77aaec927df0b7cd55c064d6e046f19cb
Tyler Pierce <pierce.josepht@gmail.com>
2026-04-22 11:40:39 -0400

    Added batch file to run main w/o execution policy issues.

---
+++ Device Setup/run.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo ============================
+echo running setup script 
+echo ============================
+echo.
+
+powershell -ExecutionPolicy Bypass -File "%~dp0main.ps1"
+
+
+echo ============================
+echo setup script finished
+echo ============================
+echo.
+echo.
+
+
+pause
